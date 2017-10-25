0c60dd80adeb76c3fc17137a5e70e21679888fc9
David Costa <S229089@studenti.polito.it>
2017-10-25 09:58:49 +0200

    ver 1

--- main.c
+++ main.c
@@ -1,17 +1,6 @@
 #include "Librerie/libreria_principale.h"
-#ifdef MAX_VETT
-#undef MAX_VETT
-#define MAX_VETT 100000
-//NON FUNZIONA NELLA LIBRERIA
-#endif
+
 int main() {
-    hello_world();
+    printf("Labroatorio 3.1:\n");
     return 0;
 }
-/*Domande sono:
-1) Ho fatto bene ?
-2) I Macro come funzionano
-3) Funzioni che accettano parametri diversi
-4) Come fare LOG
-5) Come modificare le costanti anche nella libreria? */
-
